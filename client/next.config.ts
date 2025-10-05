edaf4636547c69d741f57bb1f19b4acd15700665
Rudra Pratap singh <115483864+Rudra1106@users.noreply.github.com>
2025-10-05 16:44:12 +0530

    Update next.config.ts
    next js backend compatibility issues solution

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,8 +4,17 @@ const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
       bodySizeLimit: '10mb'
-    }
-  }
+    },
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          "https://carboncreditsanalyzer-production.up.railway.app/:path*", 
+      },
+    ];
+  },
 }
 
 export default nextConfig
